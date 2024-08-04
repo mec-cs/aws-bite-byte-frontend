d790329b33b37e07c4fedcc81cd675b65b72dc46
Enes <102901204+mec-cs@users.noreply.github.com>
2024-08-05 00:11:16 +0300

    main page ui small update

--- app/src/main/java/com/chattingapp/foodrecipeuidemo/composables/cards/CardViewFavoriteRecipes.kt
+++ app/src/main/java/com/chattingapp/foodrecipeuidemo/composables/cards/CardViewFavoriteRecipes.kt
@@ -9,12 +9,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -34,10 +36,17 @@ fun CardViewFavoriteRecipes(navController: NavController, cardId: String) {
         modifier = Modifier
             .fillMaxWidth()
             .height(150.dp)
+            .shadow(
+                elevation = 16.dp,
+                ambientColor = Color.Black,
+                spotColor = Color.Red,
+                shape = RoundedCornerShape(30.dp)
+            )
             .clickable {
                 navController.navigate("recipeCategory/$cardId")
             },
-        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
+        shape = RoundedCornerShape(30.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 24.dp)
     ) {
         Box(
             modifier = Modifier
