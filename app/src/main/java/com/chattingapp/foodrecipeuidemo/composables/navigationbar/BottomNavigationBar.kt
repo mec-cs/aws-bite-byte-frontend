c8bb9910b4d4dc6ae1a60b5a11364640ed2610d4
BatuUzun <uzunbatu@hotmail.com>
2024-08-11 21:01:58 +0300

    bottom navigation bar updated

--- app/src/main/java/com/chattingapp/foodrecipeuidemo/composables/navigationbar/BottomNavigationBar.kt
+++ app/src/main/java/com/chattingapp/foodrecipeuidemo/composables/navigationbar/BottomNavigationBar.kt
@@ -1,78 +1,129 @@
 package com.chattingapp.foodrecipeuidemo.composables.navigationbar
 
 import android.os.SystemClock
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Person
-import androidx.compose.material.icons.filled.Search
-import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.chattingapp.foodrecipeuidemo.R
 
 @Composable
 fun AppNavigationBar(navController: NavController) {
-    var lastClickTime by remember { mutableStateOf(0L) }
+    var lastClickTimeFeed by remember { mutableStateOf(0L) }
+    var lastClickTimeProfile by remember { mutableStateOf(0L) }
     val clickInterval = 10000L // 10 seconds
+    val ICON_SIZE = 28.dp
+    val ICON_SIZE_FEED = 32.dp
+    var currentRoot by remember { mutableStateOf("home") }
+
     BottomNavigation(
         backgroundColor = MaterialTheme.colors.surface,
         contentColor = MaterialTheme.colors.onSurface,
         elevation = 8.dp
     ) {
         BottomNavigationItem(
-            icon = { Icon(Icons.Filled.Home, contentDescription = "Home") },
-            selected = currentRoute(navController) == "home",
-            onClick = { navController.navigate("home") }
+            icon = {
+                Icon(
+                    painterResource(id = if (currentRoot == "home") R.drawable.homeselected else R.drawable.homenotselected),
+                    contentDescription = "Home",
+                    modifier = Modifier.size(ICON_SIZE)
+                )
+            },
+            selected = currentRoot == "home",
+            onClick = {
+                currentRoot = "home"
+
+                lastClickTimeFeed = 0
+                lastClickTimeProfile = 0
+                navController.navigate("home")
+            }
         )
 
         BottomNavigationItem(
-            icon = { Icon(Icons.Filled.Search, contentDescription = "Search") },
-            selected = currentRoute(navController) == "search",
-            onClick = { navController.navigate("search") }
+            icon = {
+                Icon(
+                    painterResource(id = if (currentRoot == "search") R.drawable.searchselected else R.drawable.searchnotselected),
+                    contentDescription = "Search",
+                    modifier = Modifier.size(ICON_SIZE)
+                )
+            },
+            selected = currentRoot == "search",
+            onClick = {
+                currentRoot = "search"
+
+                lastClickTimeProfile = 0
+                lastClickTimeFeed = 0
+                navController.navigate("search")
+            }
         )
-        BottomNavigationItem(
-            icon = { Icon(Icons.Filled.Add, contentDescription = "Create com.chattingapp.foodrecipeuidemo.composables.search.Recipe") },
-            selected = currentRoute(navController) == "create recipe",
-            onClick = { navController.navigate("create recipe") }
-        )
-        /*BottomNavigationItem(
-            icon = { Icon(Icons.Filled.ShoppingCart, contentDescription = "Feed") },
-            selected = currentRoute(navController) == "feed",
-            onClick = { navController.navigate("feed") }
-        )*/
 
         BottomNavigationItem(
-            icon = { Icon(Icons.Filled.ShoppingCart, contentDescription = "Feed") },
-            selected = currentRoute(navController) == "feed",
+            icon = {
+                Icon(
+                    painterResource(id = if (currentRoot == "create recipe") R.drawable.createrecipeselected else R.drawable.createrecipenotselected),
+                    contentDescription = "Create Recipe",
+                    modifier = Modifier.size(ICON_SIZE)
+                )
+            },
+            selected = currentRoot == "create recipe",
+            onClick = {
+                currentRoot = "create recipe"
+
+                lastClickTimeFeed = 0
+                lastClickTimeProfile = 0
+                navController.navigate("create recipe")
+            }
+        )
+
+        BottomNavigationItem(
+            icon = {
+                Icon(
+                    painterResource(id = if (currentRoot == "feed") R.drawable.feedselected else R.drawable.feednotselected),
+                    contentDescription = "Feed",
+                    modifier = Modifier.size(ICON_SIZE_FEED)
+                )
+            },
+            selected = currentRoot == "feed",
             onClick = {
                 val currentTime = SystemClock.elapsedRealtime()
-                if (currentTime - lastClickTime >= clickInterval) {
-                    lastClickTime = currentTime
+                currentRoot = "feed"
+                if (currentTime - lastClickTimeFeed >= clickInterval) {
+                    lastClickTimeFeed = currentTime
+                    lastClickTimeProfile = 0
                     navController.navigate("feed")
                 }
             }
         )
 
         BottomNavigationItem(
-            icon = { Icon(Icons.Filled.Person, contentDescription = "Profile") },
-            selected = currentRoute(navController) == "profile",
-            onClick = { navController.navigate("profile") }
-        )
-        // Add more BottomNavigationItem for additional destinations
-    }
-}
+            icon = {
+                Icon(
+                    painterResource(id = if (currentRoot == "profile") R.drawable.profileselected else R.drawable.profilenotselected),
+                    contentDescription = "Profile",
+                    modifier = Modifier.size(ICON_SIZE)
+                )
+            },
+            selected = currentRoot == "profile",
+            onClick = {
+                currentRoot = "profile"
 
-@Composable
-private fun currentRoute(navController: NavController): String? {
-    val currentRoute = navController.currentBackStackEntry?.destination?.route
-    return currentRoute
+                val currentTime = SystemClock.elapsedRealtime()
+                if (currentTime - lastClickTimeProfile >= clickInterval) {
+                    lastClickTimeProfile = currentTime
+                    lastClickTimeFeed = 0
+                    navController.navigate("profile")
+                }
+            }
+        )
+    }
 }
