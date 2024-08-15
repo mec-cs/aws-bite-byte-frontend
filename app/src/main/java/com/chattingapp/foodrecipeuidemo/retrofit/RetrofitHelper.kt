4146c16844debb8e53d39bfdb3531b0f94ad275d
BatuUzun <uzunbatu@hotmail.com>
2024-08-15 17:03:27 +0300

    api key added

--- app/src/main/java/com/chattingapp/foodrecipeuidemo/retrofit/RetrofitHelper.kt
+++ app/src/main/java/com/chattingapp/foodrecipeuidemo/retrofit/RetrofitHelper.kt
@@ -1,21 +1,38 @@
 package com.chattingapp.foodrecipeuidemo.retrofit
 
 import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.converter.scalars.ScalarsConverterFactory
 
 object RetrofitHelper {
-    var gson = GsonBuilder()
+
+    private const val BASE_URL = "http://192.168.1.100:8765/"
+    private const val API_TOKEN = "f57d10a3-f19e-4e17-8e77-c058ab937156"
+
+    // Create an OkHttpClient with an Interceptor to add the API token to requests
+    private val httpClient = OkHttpClient.Builder()
+        .addInterceptor { chain ->
+            val originalRequest = chain.request()
+            val requestWithToken = originalRequest.newBuilder()
+                .header("Authorization", "Bearer $API_TOKEN")
+                .build()
+            chain.proceed(requestWithToken)
+        }
+        .build()
+
+    private val gson = GsonBuilder()
         .setLenient()
         .create()
-    private const val BASE_URL = "http://192.168.1.100:8765/"
 
     private val retrofit: Retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
-        .addConverterFactory(ScalarsConverterFactory.create()) //important
-        .addConverterFactory(GsonConverterFactory.create(gson))
+        .client(httpClient)  // Attach OkHttpClient to Retrofit
+        .addConverterFactory(ScalarsConverterFactory.create()) // For raw responses
+        .addConverterFactory(GsonConverterFactory.create(gson)) // For JSON responses
         .build()
+
     val apiService: RetrofitAPICredentials = retrofit.create(RetrofitAPICredentials::class.java)
 }
 
